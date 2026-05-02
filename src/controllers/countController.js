22b13bd60a8d4ca71748638e0e80c5870462c0dc
Klayton COD.ias <klayton.dias@hotmail.com>
2026-05-02 07:39:41 -0400

    feat: aprimorar controle de CORS nos controladores de contagem e visitas para permitir referers autorizados

--- src/controllers/countController.js
+++ src/controllers/countController.js
@@ -4,9 +4,16 @@ const Sentry = require('../config/sentry');
 
 module.exports = async function handler(req, res) {
   const origin = req.headers['origin'];
+  const referer = req.headers['referer'] || '';
 
-  if (origin && ALLOWED_ORIGINS.includes(origin)) {
+  const originAllowed = origin && ALLOWED_ORIGINS.includes(origin);
+  const refererAllowed = !originAllowed && ALLOWED_ORIGINS.some((o) => referer.startsWith(o));
+
+  if (originAllowed) {
     res.setHeader('Access-Control-Allow-Origin', origin);
+  } else if (refererAllowed) {
+    const matchedOrigin = ALLOWED_ORIGINS.find((o) => referer.startsWith(o));
+    res.setHeader('Access-Control-Allow-Origin', matchedOrigin);
   }
   res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
@@ -15,7 +22,7 @@ module.exports = async function handler(req, res) {
   if (req.method === 'OPTIONS') return res.status(200).end();
   if (req.method !== 'GET') return res.status(405).json({ error: 'Method not allowed' });
 
-  if (!origin || !ALLOWED_ORIGINS.includes(origin)) {
+  if (!originAllowed && !refererAllowed) {
     return res.status(403).json({ error: 'Forbidden' });
   }
 
